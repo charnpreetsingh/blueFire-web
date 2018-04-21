f81621c9d45534720781f527d0eda543faa8b50c
charnpreetsingh <csingh02@calpoly.edu>
2018-04-21 07:58:12 -0700

    update prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBNhYTBNxdwQGl3UtpnqQIlNyYxFIISsi0",
+    authDomain: "bluefire-1524297464700.firebaseapp.com",
+    databaseURL: "https://bluefire-1524297464700.firebaseio.com",
+    projectId: "bluefire-1524297464700",
+    storageBucket: "",
+    messagingSenderId: "411195042824"
+  }
 };
